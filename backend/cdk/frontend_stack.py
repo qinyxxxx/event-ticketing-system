0815320fc4296f491b788a39d5bc5a9fee12b318
Yuxin Qin <qinyuxin97@gmail.com>
2025-12-07 22:15:30 -0800

    fix: cloudfront config

--- backend/cdk/frontend_stack.py
+++ backend/cdk/frontend_stack.py
@@ -5,6 +5,7 @@ from aws_cdk import (
     aws_s3_deployment as s3deploy,
     aws_cloudfront_origins as origins
 )
+from aws_cdk import aws_iam as iam
 from constructs import Construct
 
 
@@ -24,15 +25,63 @@ class FrontendStack(Stack):
         )
 
         # =========================
-        # 2. CloudFront Distribution
+        # 2. CloudFront OAC + Distribution
         # =========================
-        distribution = cloudfront.Distribution(
+        oac = cloudfront.CfnOriginAccessControl(
+            self,
+            "FrontendOAC",
+            origin_access_control_config=cloudfront.CfnOriginAccessControl.OriginAccessControlConfigProperty(
+                name="ticketing-frontend-oac",
+                origin_access_control_origin_type="s3",
+                signing_behavior="always",
+                signing_protocol="sigv4"
+            )
+        )
+
+        distribution = cloudfront.CfnDistribution(
             self,
             "FrontendDistribution",
-            default_behavior=cloudfront.BehaviorOptions(
-                origin=origins.S3Origin(frontend_bucket)
-            ),
-            default_root_object="index.html"
+            distribution_config=cloudfront.CfnDistribution.DistributionConfigProperty(
+                enabled=True,
+                default_root_object="index.html",
+                origins=[
+                    cloudfront.CfnDistribution.OriginProperty(
+                        id="S3Origin",
+                        domain_name=frontend_bucket.bucket_regional_domain_name,
+                        s3_origin_config=cloudfront.CfnDistribution.S3OriginConfigProperty(
+                            origin_access_identity=""
+                        ),
+                        origin_access_control_id=oac.attr_id
+                    )
+                ],
+                default_cache_behavior=cloudfront.CfnDistribution.DefaultCacheBehaviorProperty(
+                    target_origin_id="S3Origin",
+                    viewer_protocol_policy="redirect-to-https",
+                    allowed_methods=["GET", "HEAD", "OPTIONS"],
+                    cached_methods=["GET", "HEAD"],
+                    compress=True,
+                    forwarded_values=cloudfront.CfnDistribution.ForwardedValuesProperty(
+                        query_string=False,
+                        cookies=cloudfront.CfnDistribution.CookiesProperty(
+                            forward="none"
+                        )
+                    )
+                )
+            )
+        )
+
+        frontend_bucket.add_to_resource_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["s3:GetObject"],
+                resources=[f"{frontend_bucket.bucket_arn}/*"],
+                principals=[iam.ServicePrincipal("cloudfront.amazonaws.com")],
+                conditions={
+                    "StringEquals": {
+                        "AWS:SourceArn": f"arn:aws:cloudfront::{self.account}:distribution/{distribution.ref}"
+                    }
+                }
+            )
         )
 
         # =========================
@@ -48,4 +97,4 @@ class FrontendStack(Stack):
         )
 
         # Output a public URL
-        self.cloudfront_url = distribution.distribution_domain_name
+        self.cloudfront_url = distribution.attr_domain_name
